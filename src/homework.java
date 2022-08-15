fbb1c4e327535f2f5ac0723f694da76c63abf842
user11 <pehotskijdanil@gmail.com>
2022-08-15 17:33:27 +0200

    homework add

--- src/homework.java
+++ src/homework.java
@@ -2,45 +2,23 @@ import java.util.Scanner;
 
 public class homework {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("введите первое число");
-        int a = scanner.nextInt();
-        System.out.println(a + "первое число ");
-        System.out.println("введите знак");
-        char sing = scanner.next().charAt(0);
-        System.out.println("введите второе число");
-        int b =  scanner.nextInt();
-        System.out.println("ответ: ");
-        if (sing == '*') {
-            int result = myltiply(a, b);
-            System.out.println("ответ: "+ result);
-        }
-        if (sing == '+') {
-            int result = sum(a, b);
-            System.out.println("ответ: "+ result);
-        }
-        if (sing == '/') {
-            int result = devision(a, b);
-            System.out.println("ответ: "+ result);
-        }
-        if (sing == '-') {
-            int result = minus(a, b);
-            System.out.println("ответ: "+ result);
-        }
+        boolean isLeap = isLeapYear(2004);
+        System.out.println(isLeap);
+    }
+    public static boolean isLeapYear(int year){
+        int prescition =year % 4;
+        System.out.println(prescition);
+        return prescition == 0;
+
+
+
+
+
 
 
     }
 
 
-
-
-    public static int sum(int a, int b)  {return a + b; }
-
-    public static int minus(int a, int b)  {return a - b; }
-
-    public static int myltiply(int a, int b)  {return a * b; }
-
-    public static int devision(int a, int b)  {return a / b; }
 
 
 
